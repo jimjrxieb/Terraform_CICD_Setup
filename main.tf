25c1da6b9641ad7b2a9ff6d3a5c27a4f7cb27898
jimjrxieb <jimmie012506@gmail.com>
2024-10-12 16:26:00 -0400

    adding kubecluster

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ data "aws_ami" "ubuntu" {
 }
 
 ################################## TERRAFORM SETUP FOR EKS CLUSTER ############################
+
 resource "aws_instance" "terraform_instance" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.large"
@@ -123,6 +124,70 @@ resource "aws_instance" "nexus_instance" {
     }
   }
 }
+
+######################################## Kubernetes ########################################
+
+resource "aws_instance" "k8s_master" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = var.vpc_security_group_ids
+  tags                   = merge(var.tags, { Name = "K8_Master" })
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update -y",
+      "sudo apt-get install -y docker.io",
+      "sudo chmod 666 /var/run/docker.sock",
+      "sudo apt-get install -y apt-transport-https ca-certificates curl gnupg",
+      "sudo mkdir -p -m 755 /etc/apt/keyrings",
+      "curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg",
+      "echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list",
+      "sudo apt update -y",
+      "sudo apt install -y kubeadm kubelet kubectl"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file(var.private_key_path)
+      host        = self.public_ip
+    }
+  }
+}
+
+######################################## Kubernetes Slaves ########################################
+
+resource "aws_instance" "k8s_worker" {
+  count                  = 2
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = var.vpc_security_group_ids
+  tags = merge(var.tags, { Name = "K8_Worker_${count.index + 1}" })
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update -y",
+      "sudo apt-get install -y docker.io",
+      "sudo chmod 666 /var/run/docker.sock",
+      "sudo apt-get install -y apt-transport-https ca-certificates curl gnupg",
+      "sudo mkdir -p -m 755 /etc/apt/keyrings",
+      "curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg",
+      "echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list",
+      "sudo apt update -y",
+      "sudo apt install -y kubeadm kubelet kubectl"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file(var.private_key_path)
+      host        = self.public_ip
+    }
+  }
+}
+
 
 ######################################## JENKINS ########################################
 /*
